05ec6b64e83389b3099c51bafb148edb426c5c02
Ayeni Michael <ayenimike80@gmail.com>
2023-03-22 11:50:15 +0100

    edited live-site url

--- features/Project/components/SingleProjectCard.tsx
+++ features/Project/components/SingleProjectCard.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
-import Router from "next/router";
 
 import subString from "../../../utils/subString";
 
 import classes from "./SingleProjectCard.module.scss";
 
 import { ProjectType } from "../type";
-import { FaEllipsisH } from "react-icons/fa";
 
 const Project: React.FC<ProjectType> = ({ slug, title, tags, description }) => {
   const newArray = tags?.slice(0, 5);
@@ -23,9 +20,7 @@ const Project: React.FC<ProjectType> = ({ slug, title, tags, description }) => {
         <div className={classes.Tags}>
           {newArray?.map((tag) => (
             <span key={tag}>{tag}</span>
-          ))}{" "}
-          &nbsp;
-          <FaEllipsisH style={{ color: "#ffa2f7", marginLeft: "-1rem" }} />
+          ))}
         </div>
         <div className="pointer">
           <div className={classes.Description}>
