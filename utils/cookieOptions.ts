b14bc26802ff3703bfacecde30b9e32bdd21b507
Ayeni Michael <ayenimike80@gmail.com>
2023-03-22 10:18:35 +0100

    setting cookie options to comply with nextjs

--- utils/cookieOptions.ts
+++ utils/cookieOptions.ts
@@ -2,9 +2,9 @@ import { SetOption } from "cookies";
 
 export const CookieOptions: SetOption = {
   httpOnly: true,
-  // sameSite: "strict", // as I am hosting them on the same server
-  sameSite: process.env.NODE_ENV == "production" ? "none" : "lax",
-  // secure: true, // This works both in dev mode and prod in NODE BACKEND but DOENS'T WORK in next api.
-  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: "strict", // as I am hosting them on the same server
+  path: "/",
+  // sameSite: process.env.NODE_ENV == "production" ? "none" : "lax",
+  secure: process.env.NODE_ENV === "production",
   maxAge: +process.env.REFRESH_TOKEN_MAXAGE!,
 };
